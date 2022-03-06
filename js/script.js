b39c778a8fe96aa5e88b02392a70936f5d83786d
Kyrylbabkou <bobkovum@gmail.com>
2022-03-06 13:53:15 +0300

    Верстка и адаптация шапки

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function (){
+    $('.burger-menu').click(function () {
+        if($('nav ul').css('display') == 'none'){
+            $('nav ul').slideDown();
+        } else {
+            $('nav ul').slideUp();
+        }
+        
+    });
+});
